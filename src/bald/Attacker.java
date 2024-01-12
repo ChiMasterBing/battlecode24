da8ad910e9db62b57f9bd980d4a67616d30a99a3
Johnny <veteransmattertoo@gmail.com>
2024-01-11 23:19:14 -0500

    priority

---
+++ src/bald/Attacker.java
@@ -0,0 +1,102 @@
+package bald;
+
+import battlecode.common.*;
+
+import java.util.Map;
+import java.util.Random;
+
+public class Attacker extends Robot{
+    static final Direction[] directions = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
+    };
+    static final Random rng = new Random(6147);
+
+    public Attacker(RobotController rc) throws GameActionException {
+        super(rc);
+    }
+    public void turn() throws GameActionException{
+        super.turn();
+    }
+    public static void updateEnemyRobots() throws GameActionException{
+        // Sensing methods can be passed in a radius of -1 to automatically
+        // use the largest possible value.
+        RobotInfo[] enemyRobots = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+        if (enemyRobots.length != 0){
+            rc.setIndicatorString("There are nearby enemy robots! Scary!");
+            // Save an array of locations with enemy robots in them for future use.
+            MapLocation[] enemyLocations = new MapLocation[enemyRobots.length];
+            for (int i = 0; i < enemyRobots.length; i++){
+                enemyLocations[i] = enemyRobots[i].getLocation();
+            }
+            // Let the rest of our team know how many enemy robots we see!
+            if (rc.canWriteSharedArray(0, enemyRobots.length)){
+                rc.writeSharedArray(0, enemyRobots.length);
+                int numEnemies = rc.readSharedArray(0);
+            }
+
+        }
+    }
+    public static MapLocation findLeader() throws GameActionException {
+        RobotInfo[] friendlyRobots = rc.senseNearbyRobots(-1, rc.getTeam());
+        int maxH = rc.getHealth()*1000000+rc.getID();
+        MapLocation ret = null;
+        for(RobotInfo i: friendlyRobots){
+            if(maxH<i.getHealth()){
+                maxH = i.getHealth()*1000000+rc.getID();
+                ret = i.getLocation();
+            }
+        }
+        return ret;
+    }
+    public static MapLocation findBestAttackLocation() throws GameActionException{
+        RobotInfo[] enemyRobots = rc.senseNearbyRobots(4, rc.getTeam().opponent());
+        RobotInfo[] friendlyRobots = rc.senseNearbyRobots(4, rc.getTeam());
+        int totalHeal = 0;
+        for(RobotInfo i: enemyRobots){
+            if(i.healLevel>i.attackLevel){
+                totalHeal+=80;
+            }
+        }
+        double rounds = 0;
+        MapLocation ret = null;
+        for(RobotInfo i : enemyRobots){
+            MapLocation enemyLoc = i.getLocation();
+            int totalAttack = 0;
+            for(RobotInfo j: friendlyRobots){
+                if(j.attackLevel>j.healLevel&&j.getLocation().isWithinDistanceSquared(enemyLoc, 4)){
+                    totalAttack+=180;
+                }
+            }
+            double rnds = 0;
+            int roundstokill = 100000;
+            if(i.hasFlag){
+                rnds+=100000000;
+            }
+            if(totalAttack-totalHeal+1!=0) {
+                roundstokill = i.getHealth() / (totalAttack - totalHeal + 1);
+            }
+            rnds +=1/((double)roundstokill+1.0);
+            rnds*=1000;
+            if(i.attackLevel>=i.healLevel){
+                rnds+=(i.attackLevel+1);
+            }else{
+                rnds+=i.healLevel;
+            }
+            rnds*=1000;
+            rnds+=20-rc.getLocation().distanceSquaredTo(enemyLoc);
+            if(rnds>rounds){
+                rounds = rnds;
+                ret = i.getLocation();
+            }
+
+        }
+        return ret;
+    }
+}
---
+++ src/bald/RobotPlayer.java
@@ -0,0 +1,67 @@
+package bald;
+
+import battlecode.common.*;
+//import macroPath.Attacker;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+
+public strictfp class RobotPlayer {
+
+    /**
+     * We will use this variable to count the number of turns this robot has been alive.
+     * You can use static variables like this to save any information you want. Keep in mind that even though
+     * these variables are static, in Battlecode they aren't actually shared between your robots.
+     */
+    static int turnCount = 0;
+    //static macroPath MP;
+
+    static final Random rng = new Random(6147);
+
+    static boolean mainDuck = false;
+
+    /** Array containing all the possible movement directions. */
+    static final Direction[] directions = {
+        Direction.NORTH,
+        Direction.NORTHEAST,
+        Direction.EAST,
+        Direction.SOUTHEAST,
+        Direction.SOUTH,
+        Direction.SOUTHWEST,
+        Direction.WEST,
+        Direction.NORTHWEST,
+    };
+
+    @SuppressWarnings("unused")
+    public static void run(RobotController rc) throws GameActionException {
+        // You can also use indicators to save debug notes in replays.
+        Robot robot;
+        robot = new Attacker(rc);
+        MapLocation currentTarget = null;
+        while(true){
+            rc.setIndicatorString("Hello world!");
+            if (rc.getRoundNum() == 2) {
+                robot.populateTeamIDS();
+            }
+            if (!rc.isSpawned()){
+                if (robot.myMoveNumber < 10) {
+                    MapLocation[] spawnLocs = rc.getAllySpawnLocations();
+                    // Pick a random spawn location to attempt spawning in.
+                    MapLocation randomLoc = spawnLocs[robot.myMoveNumber];
+                    if (rc.canSpawn(randomLoc)) rc.spawn(randomLoc);
+                }
+            }else {
+
+                robot.turn();
+                
+            }
+            Clock.yield();
+        }
+        // Your code should never reach here (unless it's intentional)! Self-destruction imminent...
+    }
+}
